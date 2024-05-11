325de73ff74433f9dee4c50e780eea0067ff3129
Daniel Szulc <szulc.d@gmail.com>
2024-05-11 13:46:05 +0200

    Update helper_functions.py
    walk_through_dir added

--- helper_functions.py
+++ helper_functions.py
@@ -2,8 +2,14 @@
 Helper functions code for TensorFlow Certficate Course 
 """
 import zipfile
+import os
 
 def unzip_data(filepath):
     zip_handler = zipfile.ZipFile(filepath)
     zip_handler.extractall()
     zip_handler.close()
+
+def walk_through_dir(path_to_dir):
+    for dirpath, dirnames, filenames in os.walk(path_to_dir):
+        print(f"There is {len(dirnames)} dirs and {len(filenames)} in '{dirpath}'."
+    
